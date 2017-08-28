567e2eac6440eec4be2de46d72bd9900ba548ae7
Bhavani Shankar <ebhavanishankar@gmail.com>
2017-08-28 15:12:15 +0530

    update and add missing functions for list.js

--- chapter4/list.js
+++ chapter4/list.js
@@ -1,11 +1,16 @@
+function prepend(tail, value) {
+    let head = {};
+    head.value = value;
+    head.rest = tail;
+    tail = head;
+    return tail;
+}
+
 function arrayToList(arr) {
-    let list = {}, tail = {};
-    for (let i = arr.length - 1; i >= 0; --i) {
-        list = {};
-        list.value = arr[i];
-        list.rest = tail;
-        tail = list;
-    }
+    let list = {};
+    for (let i = arr.length - 1; i >= 0; --i)
+        list = prepend(list, arr[i]);
+
     return list;
 }
 
@@ -20,7 +25,18 @@ function listToArray(list) {
 }
 
 
-let arr = [1, 2, 3, 4, 5];
+function nth(list, n) {
+    if (!list)
+        return undefined;
+    if (n == 0)
+        return list.value;
+    return nth(list.rest, n - 1);
+}
+
+
+let arr = [1, 2, 3];
 let list = arrayToList(arr);
 console.log(list);
 console.log(listToArray(list));
+
+console.log(nth(list, 1));
